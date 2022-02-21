d3c09c1855345ff387f421423227686f1bc5ee5d
Byaxy <charlesbyaxy@gmail.com>
2022-02-21 05:54:00 -0800

    fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,9 +6,10 @@ import sys
 
 
 def number_of_subscribers(subreddit):
-    '''gets the titles of the first 10 hot posts listed for a given subreddit'''
+    '''prints titles of the first 10 hot posts listed for a given subreddit'''
     headers = {'User-agent': 'hbnbtest'}
-    posts = requests.get('https://www.reddit.com/r/{}/about.json?limit=10'.format(
+    url = 'https://www.reddit.com/r/'
+    posts = requests.get(url + '{}/hot.json?limit=10'.format(
         sys.argv[1]), allow_redirects=False, headers=headers)
 
     if posts.status_code == 200:
