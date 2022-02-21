0e73a153f919c922c581e63f84c8d25698c45a90
Byaxy <charlesbyaxy@gmail.com>
2022-02-21 05:45:49 -0800

    fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,10 +8,11 @@ import sys
 def number_of_subscribers(subreddit):
     '''gets the titles of the first 10 hot posts listed for a given subreddit'''
     headers = {'User-agent': 'hbnbtest'}
-    subs = requests.get('https://www.reddit.com/r/{}/about.json?limit=10'.format(
+    posts = requests.get('https://www.reddit.com/r/{}/about.json?limit=10'.format(
         sys.argv[1]), allow_redirects=False, headers=headers)
 
-    if subs.status_code == 200:
-        return (subs.json()['data']['subscribers'])
+    if posts.status_code == 200:
+        for post in posts.json()['data']['children']:
+            print(post['data']['title'])
     else:
-        return 0
+        print(None)
